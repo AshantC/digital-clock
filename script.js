7ae7d3a7c4ef2a2ed10675e6a2829b90002472e0
AshantC <ashantchaudhary22@gmail.com>
2024-03-03 08:17:57 +0545

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function curTime(){
+    let now = new Date();
+    let h = now.getHours();
+    let m = now.getMinutes();
+    let s = now.getSeconds();
+    let am_pm = "AM"
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+    if (h > 12){
+        h = h - 12
+        am_pm = "PM";
+    }
+
+    document.querySelector("#hours").innerHTML = h;
+    document.querySelector("#minutes").innerHTML = m;
+    document.querySelector("#seconds").innerHTML = s;
+    // document.querySelector("#am_pm").innerHTML = am_pm;
+
+    setTimeout(curTime, 1000);
+}
+curTime();
