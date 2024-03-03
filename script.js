489bb5117de0b25750dafff75d5ce2338051ee1a
AshantC <ashantchaudhary22@gmail.com>
2024-03-03 14:47:12 +0545

    blinking added

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function curTime(){
     let m = now.getMinutes();
     let s = now.getSeconds();
     let am_pm = "AM"
+    let blink = document.querySelector('#blink');
 
     h = h < 10 ? "0" + h : h;
     m = m < 10 ? "0" + m : m;
@@ -12,12 +13,14 @@ function curTime(){
     if (h > 12){
         h = h - 12
         am_pm = "PM";
+        h = h < 10 ? "0" + h : h;
     }
 
     document.querySelector("#hours").innerHTML = h;
     document.querySelector("#minutes").innerHTML = m;
     // document.querySelector("#seconds").innerHTML = s;
     document.querySelector("#am_pm").innerHTML = am_pm;
+    
 
     setTimeout(curTime, 1000);
 }
